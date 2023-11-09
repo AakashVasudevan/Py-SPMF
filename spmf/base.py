192cc3fb9e408f99dd42b3c59f778a5aba0609ae
Aakash Vasudevan <Aakash.Vasudevan@bluemarvel.ai>
2023-11-09 13:36:00 -0700

    Debug file paths and temp file creation/deletion

--- spmf/base.py
+++ spmf/base.py
@@ -1,34 +1,35 @@
-""" 
+"""
 Python 3 Wrapper for SPMF
 http://www.philippe-fournier-viger.com/spmf
 
 """
 
-from typing import Dict, Text, List, Any
-from pathlib import Path
+import os
+import subprocess
+import tempfile
 from abc import ABC, abstractmethod
+from typing import Any, List, Text
 
 import pandas as pd
-import os
-import tempfile
-import subprocess
+
 
 class Spmf(ABC):
     """ Abstract Base Class for SPMF Wrapper """
+
     def __init__(self, memory: int = 1024, output_file_name: Text = 'output.txt') -> None:
-        """ Initialize Object 
+        """ Initialize Object
 
         :param memory:
         """
-        self.executable_path = 'binaries\\spmf.jar'
+        self.executable_path = os.path.join('binaries', 'spmf.jar')
         self.output_file_name = output_file_name
         self.memory = memory
-    
+
     @abstractmethod
     def _parse_input_dataframe(self, input_df: pd.DataFrame) -> Text:
         """ Convert Pandas Dataframe to input string """
         pass
-    
+
     @abstractmethod
     def _parse_output_file(self, **kwargs) -> Any:
         """ Parse output txt file created by SPMF algorithm """
@@ -38,7 +39,7 @@ class Spmf(ABC):
     def _create_output_dataframe(self) -> pd.DataFrame:
         """ Create Pandas Dataframe from SPMF output text file """
         pass
-    
+
     @abstractmethod
     def _create_subprocess_arguments(self, input_file_name: Text) -> List:
         """ Create arguments list to pass to subprocess """
@@ -47,34 +48,33 @@ class Spmf(ABC):
     def run_pandas(self, input_df: pd.DataFrame) -> pd.DataFrame:
         """ Run SPMF algorithm on Pandas Dataframe
 
-        :param input_df: Input Dataframe            
+        :param input_df: Input Dataframe
         :return: Output Dataframe
         """
         input_file = self._convert_dataframe_to_file_object(input_df)
         self.run(input_file.name)
         self._delete_temp_file(input_file)
         return self._create_output_dataframe(*self._parse_output_file(delete=True))
-    
+
     def run_file(self, input_file_name: Text) -> Any:
         """ Run SPMF algorithm on an input txt file
 
         :param input_file_name: Input txt file name
         :return: Results of the SPMF algorithm parsed from output file
         """
-        
-        self.run(input_file_name)        
+
+        self.run(input_file_name)
         return self._parse_output_file(delete=True)
-    
+
     def run(self, input_file_name: Text) -> None:
         """ Run SPMF Algorithm """
 
         process_arguments = self._create_subprocess_arguments(input_file_name)
         process = subprocess.check_output(process_arguments)
 
-        if "java.lang.IllegalArgumentException" in process.decode():
-            raise TypeError("java.lang.IllegalArgumentException")
-        
-    
+        if 'java.lang.IllegalArgumentException' in process.decode():
+            raise TypeError('java.lang.IllegalArgumentException')
+
     def _convert_dataframe_to_file_object(self, input_df: pd.DataFrame) -> tempfile:
         """ Convert input dataframe to text file object
 
@@ -86,15 +86,15 @@ class Spmf(ABC):
         )
 
     def _read_output_file(self, delete: bool = False) -> List[Text]:
-        """ Read output txt file created by SPMF into a list 
-        
+        """ Read output txt file created by SPMF into a list
+
         :param delete: Set to True to delete the output file after reading
         :return: List containing each line in the output file as Text
         """
 
         with open(self.output_file_name, 'r') as fp:
             lines = fp.readlines()
-        
+
         if delete:
             os.remove(self.output_file_name)
 
@@ -110,8 +110,9 @@ class Spmf(ABC):
         """
         temp_file = tempfile.NamedTemporaryFile(suffix=file_extension, delete=False)
         temp_file.write(bytes(input, 'UTF-8'))
+        temp_file.flush()
         return temp_file
-    
+
     @staticmethod
     def _delete_temp_file(temp_file: tempfile) -> None:
         """ Delete temporary file
@@ -119,12 +120,4 @@ class Spmf(ABC):
         :param temp_file: tempfile object to delete
         """
         temp_file.close()
-        os.unlink(temp_file)
-    
-
-
-
-    
-
-
-
+        os.unlink(temp_file.name)
