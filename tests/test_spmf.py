2cd2921c2dac795d81b243e84d30211487ace128
Aakash Vasudevan <Aakash.Vasudevan@bluemarvel.ai>
2023-11-09 01:36:37 -0700

    Update path to test file

--- tests/test_spmf.py
+++ tests/test_spmf.py
@@ -2,9 +2,9 @@
 
 from spmf.episode import TKE
 
-def test_read_file() -> None:
+def test_run_file() -> None:
     """ Test Read Input File """
     tke = TKE(k=6, max_window=2, timestamp_present=True)
-    result = tke.run_file('contextEMMA.txt')
+    result = tke.run_file('tests\\test_files\\contextEMMA.txt')
     print(result)
     assert len(result) > 0
