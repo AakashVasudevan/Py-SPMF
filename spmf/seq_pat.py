cbf407308975e41bb2c9c8208b23beebab7889ae
Aakash Vasudevan <Aakash.Vasudevan@bluemarvel.ai>
2023-11-08 23:59:36 -0700

    Start implementation of sequential pattern mining wrapper

--- spmf/seq_pat.py
+++ spmf/seq_pat.py
@@ -7,13 +7,13 @@ from spmf.base import Spmf
 
 class SeqPat(Spmf):
     """ Sequential Pattern Mining """
-    def __init__(self, input_df: pd.DataFrame, **kwargs) -> None:
+    def __init__(self, **kwargs) -> None:
         """ Initialize Object
         
         :param input_df: Input Dataframe
         :param kwargs: Keyword arguments to base SPMF. E.g., memory
         """
         super().__init__(**kwargs)
-        self.input = self._parse_input_dataframe(input_df)
-    
+        
+
     
