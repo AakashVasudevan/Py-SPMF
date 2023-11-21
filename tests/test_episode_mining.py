af23b17f2008b537b1b503ff0afb322423c713d7
Aakash Vasudevan <Aakash.Vasudevan@bluemarvel.ai>
2023-11-21 15:49:49 -0700

    Implement tests for maxfem and nonepi

--- tests/test_episode_mining.py
+++ tests/test_episode_mining.py
@@ -5,7 +5,7 @@ from typing import Text
 
 import pandas as pd
 
-from spmf.episode import AFEM, EMMA, TKE, EMMARules, TKERules
+from spmf.episode import AFEM, EMMA, NONEPI, TKE, EMMARules, MaxFEM, TKERules
 
 test_file_path = os.path.join('tests', 'test_files', 'contextEMMA.txt')
 
@@ -139,3 +139,51 @@ def test_afem_example_pandas() -> None:
     assert len(output) > 0
     assert set(output['Frequent episode'].to_list()) == {'1', '2', '1 2', '1 -> 1', '1 -> 2', '1 -> 1 2'}
     assert set(output['Support'].to_list()) == {5, 3, 2, 3, 2, 2}
+
+
+def test_maxfem_example_file() -> None:
+    """ Test MaxFEM on given example in
+        https://www.philippe-fournier-viger.com/spmf/MAXFEM_MAXIMAL_EPISODE_MINING.php
+    """
+    maxfem = MaxFEM(min_support=2, max_window=2, timestamp_present=True)
+    patterns, support = maxfem.run_file(test_file_path)
+    assert len(patterns) > 0 and len(support) > 0
+    assert set(patterns) == {'1 -> 1 2'}
+    assert set(support) == {2}
+
+
+def test_maxfem_example_pandas() -> None:
+    """ Test MaxFEM on given example in
+        https://www.philippe-fournier-viger.com/spmf/MAXFEM_MAXIMAL_EPISODE_MINING.php
+    """
+    maxfem = MaxFEM(min_support=2, max_window=2, timestamp_present=True)
+    mock_df = create_mock_dataframe(test_file_path)
+    output = maxfem.run_pandas(mock_df)
+    assert len(output) > 0
+    assert set(output['Frequent episode'].to_list()) == {'1 -> 1 2'}
+    assert set(output['Support'].to_list()) == {2}
+
+
+def test_nonepi_example_file() -> None:
+    """ Test NONEPI on given example in
+        https://www.philippe-fournier-viger.com/spmf/NONEPI_episode_rules.php
+    """
+    nonepi = NONEPI(min_support=2, min_confidence=0.2)
+    patterns, support, confidence = nonepi.run_file(test_file_path)
+    assert len(patterns) > 0 and len(support) > 0 and len(confidence) > 0
+    assert set(patterns) == {f'{{1}} ==> {{1}}{{1 2}}'}
+    assert set(support) == {3}
+    assert set(confidence) == {0.6666667}
+
+
+def test_nonepi_example_pandas() -> None:
+    """ Test NONEPI on given example in
+        https://www.philippe-fournier-viger.com/spmf/NONEPI_episode_rules.php
+    """
+    nonepi = NONEPI(min_support=2, min_confidence=0.2)
+    mock_df = create_mock_dataframe(test_file_path)
+    output = nonepi.run_pandas(mock_df)
+    assert len(output) > 0
+    assert set(output['Frequent episode'].to_list()) == {f'{{1}} ==> {{1}}{{1 2}}'}
+    assert set(output['Support'].to_list()) == {3}
+    assert set(output['Confidence'].to_list()) == {0.6666667}
